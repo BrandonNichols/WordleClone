b98865fac59a00280a1adbbc5b862c28e34bcf66
Brandon Nichols <brandonaronnichols@gmail.com>
2022-02-19 21:57:42 -0600

    added animation that squished letters when word was found in dictionary

--- wordle-clone/src/component/InputArea.js
+++ wordle-clone/src/component/InputArea.js
@@ -29,6 +29,20 @@ const WrongAnswerAnimation = keyframes`
   }
 `;
 
+const RightAnswerAnimation = keyframes`
+  0%{
+    transform: scaleY(1)
+  }
+
+  50%{
+    transform: scaleY(0)
+  }
+
+  100%{
+    transform: scaleY(1)
+  }
+`;
+
 const LetterContainer = styled.div`
   border: 5px solid black;
   margin: 5px 10px;
@@ -36,15 +50,32 @@ const LetterContainer = styled.div`
   font-size: 2rem;
   text-transform: uppercase;
   min-width: 2rem;
-  animation: ${(props) =>
-      props.canAnimate && props.animate ? WrongAnswerAnimation : "none"}
-    0.2s;
+  animation-name: ${(props) => {
+    switch (props.animate) {
+      case "wrong":
+        if (props.currentGuess) {
+          return WrongAnswerAnimation;
+        } else {
+          return "none";
+        }
+      case "right":
+        if (props.canAnimate) {
+          return RightAnswerAnimation;
+        } else {
+          return "none";
+        }
+      default:
+        return "none";
+    }
+  }};
+  animation-duration: ${(props) => props.duration};
+  animation-delay: ${(props) => props.delay};
 `;
 
 const InputArea = (props) => {
   const [input, setInput] = useState("");
   const [guess, setGuess] = useState([]);
-  const [animate, setAnimate] = useState(false);
+  const [animate, setAnimation] = useState("");
 
   const handleInput = (e) => {
     if (e.target.value.match(/\W|\d/)) {
@@ -76,13 +107,40 @@ const InputArea = (props) => {
   };
 
   const handleKeyDown = (e) => {
-    console.log("KEY: ", e.key);
     if (e.key === "Enter") {
       e.preventDefault();
-      setAnimate(true);
-      setInput("");
-      const submitButton = document.querySelector('button[type="submit"]');
-      submitButton.click();
+
+      if (input in props.dictionary) {
+        setInput("");
+        const submitButton = document.querySelector('button[type="submit"]');
+        submitButton.click();
+        setAnimation("right");
+      } else {
+        setAnimation("wrong");
+      }
+    }
+  };
+
+  const letterAnimationEnd = (e, canAnimate, animationDelay) => {
+    if (canAnimate) {
+      setTimeout(function () {
+        document.querySelector("input[type='text']").disabled = false;
+        reFocus();
+      }, 300);
+
+      const nextLetter = e.nextElementSibling;
+      if (nextLetter) {
+        nextLetter.animate(
+          [
+            { transform: "scaleY(1)" },
+            { transform: "scaleY(0)" },
+            { transform: "scaleY(1)" }
+          ],
+          { duration: 1000, delay: animationDelay }
+        );
+
+        letterAnimationEnd(nextLetter, canAnimate, animationDelay + 1000);
+      }
     }
   };
 
@@ -107,22 +165,22 @@ const InputArea = (props) => {
                 return (
                   <LetterContainer
                     key={index}
-                    onAnimationStart={() =>
-                      (document.querySelector(
+                    onAnimationStart={(e) => {
+                      document.querySelector(
                         "input[type='text']"
-                      ).disabled = true)
-                    }
-                    onAnimationEnd={() => {
-                      setAnimate(false);
-                      setTimeout(function () {
-                        document.querySelector(
-                          "input[type='text']"
-                        ).disabled = false;
-                        reFocus();
-                      }, 300);
+                      ).disabled = true;
+
+                      letterAnimationEnd(
+                        e.target,
+                        guessIndex + 1 === guess.length,
+                        1000
+                      );
                     }}
-                    animate={animate}
+                    animate={animate && index === 0 ? "right" : "none"}
+                    currentGuess={false}
                     canAnimate={guessIndex + 1 === guess.length}
+                    duration="1s"
+                    delay="1s"
                   >
                     {character}
                   </LetterContainer>
@@ -133,7 +191,29 @@ const InputArea = (props) => {
         })}
         <GuessContainer>
           {input.split("").map((character, index) => {
-            return <LetterContainer key={index}>{character}</LetterContainer>;
+            return (
+              <LetterContainer
+                key={index}
+                onAnimationStart={() =>
+                  (document.querySelector("input[type='text']").disabled = true)
+                }
+                onAnimationEnd={() => {
+                  setAnimation("");
+                  setTimeout(function () {
+                    document.querySelector(
+                      "input[type='text']"
+                    ).disabled = false;
+                    reFocus();
+                  }, 300);
+                }}
+                animate={animate}
+                currentGuess={true}
+                duration="0.2s"
+                delay="0s"
+              >
+                {character}
+              </LetterContainer>
+            );
           })}
         </GuessContainer>
         <button onClick={buttonClicked}>W</button>
